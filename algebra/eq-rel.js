7433089eeb5de9dc47abda524160732b7c73a056
eyamil <sid.limaye11@gmail.com>
2021-01-20 13:48:43 -0800

    Create eq-rel.js

---
+++ algebra/eq-rel.js
@@ -0,0 +1,13 @@
+class Equivalence_Relation {
+
+    /*
+    equals;
+    */
+
+    constructor(eq = (x, y) => (x == y)) {
+        this.equals = eq;
+    }
+
+}
+
+export default Equivalence_Relation;
